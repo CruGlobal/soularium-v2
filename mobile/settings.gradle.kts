54baa090f121a7f26bf461ee0fbb6eb6bea7ce4f
Daniel Bisgrove <daniel.bisgrove@cru.org>
2026-05-20 17:42:56 -0400

    chore: initialize KMP project structure (Task 1)

---
+++ mobile/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "soularium"
+
+// Modules will be added back as they're created in Phase 0.
+// include(":composeApp", ":domain", ":data")
